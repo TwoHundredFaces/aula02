71a98ae5d855a483a9486f1981d56fe74df77702
TwoHundredFaces <emersonsopares141@gmail.com>
2023-02-28 11:24:58 -0300

    base projeto mudar fundo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById('botao');
+var click = true;
+
+if (click){
+        document.body.style.background = '#35E6F3';
+    
+}else{
+    document.body.style.background = 'orange';
+    click=false;
+}
