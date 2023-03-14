32ef964823327cb8ea2ec19d41337c7f027c355d
TwoHundredFaces <emersonsopares141@gmail.com>
2023-03-14 09:25:07 -0300

    Projeto mudar fundo finalizado

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 let button = document.getElementById('botao');
-var click = true;
+let click = true;
 
-if (click){
+button.addEventListener("click", function mudar_fundo() {
+    if (click){
         document.body.style.background = '#35E6F3';
+        click=false;
+        button.style.backgroundColor = '#FFFFFF';
+        button.style.color = '#35E6F3';
     
-}else{
-    document.body.style.background = 'orange';
-    click=false;
-}
+    }else{
+    document.body.style.background = '#FFFFFF';
+    click=true;
+    button.style.backgroundColor = '#35E6F3';
+    button.style.color = '#FFFFFF';
+
+    }
+
+});
